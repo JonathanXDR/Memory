acf26c1189dec06e457e531ba16513d2f7c4faba
Jonathan Russ <jonathan.russ@swisscom.com>
2022-08-08 18:13:55 +0200

    feat(styles): update styles to use sdx

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -3,11 +3,32 @@
     <!-- <LoadingSpinner v-if="loading" /> -->
     <div class="game-header">
       <div class="score">
-        <p>Score:</p>
-        <p>Time:</p>
+        <!-- <p>Score: {{ Score }}</p>
+        <p>Time: {{ Time }}</p> -->
       </div>
       <h1>Memory</h1>
       <sdx-button label="Reset"></sdx-button>
+    </div>
+    <div class="cards row">
+      <card class="col-xs-12 col-md-3 col-xs-3 margin-bottom-3"></card>
+      <card class="col-xs-12 col-md-3 col-xs-3 margin-bottom-3"></card>
+      <card class="col-xs-12 col-md-3 col-xs-3 margin-bottom-3"></card>
+      <card class="col-xs-12 col-md-3 col-xs-3 margin-bottom-3"></card>
+
+      <card class="col-xs-12 col-md-3 col-xs-3 margin-bottom-3"></card>
+      <card class="col-xs-12 col-md-3 col-xs-3 margin-bottom-3"></card>
+      <card class="col-xs-12 col-md-3 col-xs-3 margin-bottom-3"></card>
+      <card class="col-xs-12 col-md-3 col-xs-3 margin-bottom-3"></card>
+
+      <card class="col-xs-12 col-md-3 col-xs-3 margin-bottom-3"></card>
+      <card class="col-xs-12 col-md-3 col-xs-3 margin-bottom-3"></card>
+      <card class="col-xs-12 col-md-3 col-xs-3 margin-bottom-3"></card>
+      <card class="col-xs-12 col-md-3 col-xs-3 margin-bottom-3"></card>
+
+      <card class="col-xs-12 col-md-3 col-xs-3 margin-bottom-3"></card>
+      <card class="col-xs-12 col-md-3 col-xs-3 margin-bottom-3"></card>
+      <card class="col-xs-12 col-md-3 col-xs-3 margin-bottom-3"></card>
+      <card class="col-xs-12 col-md-3 col-xs-3 margin-bottom-3"></card>
     </div>
   </div>
 </template>
