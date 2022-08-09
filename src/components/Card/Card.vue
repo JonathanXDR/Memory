841b1ba1b19f4e1e08b2298705d417f88e74531a
Jonathan Russ <jonathan.russ@swisscom.com>
2022-08-09 10:53:35 +0200

    fix(component): update depricated directive & remove aria-level

--- src/components/Card/Card.vue
+++ src/components/Card/Card.vue
@@ -1,10 +1,9 @@
 <template>
-  <div id="card" class="memory-card" v-on:click="myFilter" v-bind:class="{ active: isActive }">
-    <sdx-card ref="sdx-card-front" class="front" label-aria-level="3" background="grey"></sdx-card>
+  <div id="card" class="memory-card" @click="myFilter" v-bind:class="{ active: isActive }">
+    <sdx-card ref="sdx-card-front" class="front" background="grey"></sdx-card>
     <sdx-card
       ref="sdx-card-back"
       class="back"
-      label-aria-level="3"
       background="grey"
       image-src="https://picsum.photos/200"
     ></sdx-card>
