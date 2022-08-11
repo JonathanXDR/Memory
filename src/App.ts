128a8da7a7ae1102e3e260e9faabe5973dc9ecc0
Jonathan Russ <jonathan.russ@swisscom.com>
2022-08-11 17:28:25 +0200

    fix(sdx): add Vue.configs for logs in console

--- src/App.ts
+++ src/App.ts
@@ -1,9 +1,13 @@
 import Vue from 'vue';
+import LoadingSpinner from './components/LoadingSpinner/LoadingSpinner.vue';
 import Navbar from './components/Navbar/Navbar.vue';
+import Card from './components/Card/Card.vue';
 
 export default Vue.extend({
   name: 'App',
   components: {
+    LoadingSpinner,
     Navbar,
+    Card,
   },
 });
