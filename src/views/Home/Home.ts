792a85e821467ebef35205a5c973d3f01a97b217
Jonathan Russ <jonathan.russ@swisscom.com>
2022-08-11 17:25:08 +0200

    feat(component): add basic methods & api fetch to home component

--- src/views/Home/Home.ts
+++ src/views/Home/Home.ts
@@ -1,14 +1,151 @@
 import Vue from 'vue';
 import LoadingSpinner from '../../components/LoadingSpinner/LoadingSpinner.vue';
+import Navbar from '../../components/Navbar/Navbar.vue';
 import Card from '../../components/Card/Card.vue';
+import axios from 'axios';
+import moment from 'moment';
 
 export default Vue.extend({
   name: 'Home',
   components: {
     LoadingSpinner,
+    Navbar,
     Card,
   },
   data() {
-    return {};
+    return {
+      loading: true,
+      showModal: false,
+      cards: [] as any,
+      started: false,
+      startTime: 0 as any,
+      turns: 0,
+      flipBackTimer: null as any,
+      timer: null as any,
+      time: '--:--',
+      score: 0,
+    };
+  },
+  // TODO: Update axios with base url etc.
+  async created() {
+    const response = await axios.get('https://memory-api.dev-scapp.swisscom.com/cards');
+    const cards = response.data.sort(() => 0.5 - Math.random()).slice(0, 8);
+    this.cards = cards.concat(cards).sort(() => 0.5 - Math.random());
+    this.resetGame();
+  },
+  mounted() {
+    this.loading = false;
+  },
+  methods: {
+    resetGame() {
+      this.showModal = false;
+      const cards = this.cards;
+      this.turns = 0;
+      this.score = 0;
+      this.started = false;
+      this.startTime = 0;
+
+      cards.forEach((card: any) => {
+        card.flipped = false;
+        card.found = false;
+      });
+
+      this.cards = cards;
+    },
+
+    flippedCards() {
+      return this.cards.filter((card: any) => card.flipped);
+    },
+
+    sameFlippedCard() {
+      const flippedCards = this.flippedCards();
+      if (flippedCards.length == 2) {
+        if (flippedCards[0].id == flippedCards[1].id) {
+          return true;
+        }
+      }
+    },
+
+    setCardFounds() {
+      this.cards.forEach((card: any) => {
+        if (card.flipped) {
+          card.found = true;
+        }
+      });
+    },
+
+    checkAllFound() {
+      const foundCards = this.cards.filter((card: any) => card.found);
+      if (foundCards.length == this.cards.length) {
+        return true;
+      }
+    },
+
+    startGame() {
+      this.started = true;
+      this.startTime = moment();
+
+      this.timer = setInterval(() => {
+        this.time = moment(moment().diff(this.startTime)).format('mm:ss');
+      }, 1000);
+    },
+
+    finishGame() {
+      this.started = false;
+      clearInterval(this.timer);
+      const score =
+        1000 -
+        (moment().diff(this.startTime, 'seconds') - this.cards.length * 5) * 3 -
+        (this.turns - this.cards.length) * 5;
+      this.score = Math.max(score, 0);
+      this.showModal = true;
+    },
+
+    flipCard(card: any) {
+      if (card.found || card.flipped) {
+        console.log('Card clicked!', card);
+        return;
+      }
+
+      const flipCount = this.flippedCards().length;
+      if (flipCount == 0) {
+        card.flipped = !card.flipped; // set flipped to true
+      } else if (flipCount == 1) {
+        card.flipped = !card.flipped; // set flipped to true
+        this.turns += 1;
+
+        if (this.sameFlippedCard()) {
+          // Match!
+          this.flipBackTimer = setTimeout(() => {
+            this.clearFlipBackTimer();
+            this.setCardFounds();
+            this.clearFlips();
+
+            if (this.checkAllFound()) {
+              this.finishGame();
+            }
+          }, 200);
+
+          console.log('Match!');
+        } else {
+          // Wrong match
+          this.flipBackTimer = setTimeout(() => {
+            this.clearFlipBackTimer();
+            this.clearFlips();
+          }, 1000);
+
+          console.log('Wrong match!');
+        }
+      }
+    },
+
+    clearFlips() {
+      this.cards.map((card: any) => (card.flipped = false));
+    },
+
+    clearFlipBackTimer() {
+      clearTimeout(this.flipBackTimer);
+      this.flipBackTimer = null;
+    },
   },
 });
--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,35 +1,57 @@
 <template>
   <div id="home" class="container">
-    <!-- <LoadingSpinner v-if="loading" /> -->
-    <div class="game-header row flex-items-xs-between flex-items-xs-middle margin-v-4">
+    <LoadingSpinner v-if="loading" />
+
+    <div
+      class="game-header divider row flex-items-xs-between flex-items-xs-middle margin-v-4 padding-bottom-4"
+    >
       <div class="score">
-        <p>Score:</p>
-        <p>Time:</p>
+        <p class="margin-bottom-0 font--semi-bold">Time: {{ time }}</p>
+        <p class="margin-bottom-0 font--semi-bold">Turns: {{ turns }}</p>
       </div>
-      <h1 class="hidden-xs-down margin-0">Memory</h1>
-      <sdx-button label="Reset"></sdx-button>
+      <h1 class="hidden-sm-down margin-0">Memory</h1>
+      <sdx-button label="Reset" @click="resetGame()"></sdx-button>
     </div>
     <div class="cards row flex-items-xs-center full-width--mobile-only">
-      <card class="col-xs-auto margin-bottom-1"></card>
-      <card class="col-xs-auto margin-bottom-1"></card>
-      <card class="col-xs-auto margin-bottom-1"></card>
-      <card class="col-xs-auto margin-bottom-1"></card>
-
-      <card class="col-xs-auto margin-bottom-1"></card>
-      <card class="col-xs-auto margin-bottom-1"></card>
-      <card class="col-xs-auto margin-bottom-1"></card>
-      <card class="col-xs-auto margin-bottom-1"></card>
-
-      <card class="col-xs-auto margin-bottom-1"></card>
-      <card class="col-xs-auto margin-bottom-1"></card>
-      <card class="col-xs-auto margin-bottom-1"></card>
-      <card class="col-xs-auto margin-bottom-1"></card>
-
-      <card class="col-xs-auto margin-bottom-1"></card>
-      <card class="col-xs-auto margin-bottom-1"></card>
-      <card class="col-xs-auto margin-bottom-1"></card>
-      <card class="col-xs-auto margin-bottom-1"></card>
+      <card
+        class="memory-card col-xs-auto margin-bottom-1"
+        v-for="(card, index) in cards"
+        :card="card"
+        :key="index"
+        :class="{ flipped: card.flipped, found: card.found }"
+        @click.native="flipCard(card)"
+      ></card>
     </div>
+
+    <sdx-dialog
+      label="Congratulations! You won!"
+      type="closable-modal"
+      id="closable-modal-example"
+      display-change-callback="
+    if (arguments[0] === 'open') document.querySelector('#first-input-element').doFocus();
+    if (arguments[0] === 'closing') document.querySelector('#closable-modal-opener').doFocus();
+  "
+    >
+      <sdx-dialog-toggle v-if="showModal">
+        <sdx-button id="closable-modal-opener" label="Open closable modal"></sdx-button>
+      </sdx-dialog-toggle>
+
+      <sdx-dialog-content>
+        <p>Score: {{ score }}</p>
+        <p>
+          <sdx-input id="first-input-element" label="Enter name"></sdx-input>
+        </p>
+
+        <sdx-button-group>
+          <sdx-button label="New game" @click="resetGame()"></sdx-button>
+          <sdx-button
+            label="Cancel"
+            theme="secondary"
+            onclick="document.getElementById('closable-modal-example').close()"
+          ></sdx-button>
+        </sdx-button-group>
+      </sdx-dialog-content>
+    </sdx-dialog>
   </div>
 </template>
 
