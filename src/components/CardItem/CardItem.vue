5c6e74022da84a047b829edfd7be6c00ef9f234e
Jonathan Russ <jonathan.russ@swisscom.com>
2022-08-19 13:14:17 +0200

    fix(component): remove comment from CardItem

--- src/components/CardItem/CardItem.vue
+++ src/components/CardItem/CardItem.vue
@@ -2,20 +2,6 @@
   <div class="card-item" :class="{ flipped: card.flipped, found: card.found }">
     <div class="back" :style="`background-image: url('${card.url}')`"></div>
     <div class="front" @click="$emit('clickCallback')"></div>
-    <!-- <sdx-card
-      v-if="true"
-      class="back"
-      background="grey"
-      object-fit="contain"
-      :image-src="card.url"
-      :image-alt="card.title"
-    ></sdx-card>
-    <sdx-card
-      @click="$emit('clickCallback')"
-      v-else
-      class="front component"
-      background="grey"
-    ></sdx-card> -->
   </div>
 </template>
 <script src="./CardItem.ts" lang="ts"></script>
