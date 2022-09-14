1afd71dddf1b0d088d092c6c2688fc1a977f42b0
Jonathan Russ <jonathan.russ@swisscom.com>
2022-09-14 08:03:50 +0200

    fix(sort): update foreach with map

--- src/views/ScoreboardView/ScoreboardView.ts
+++ src/views/ScoreboardView/ScoreboardView.ts
@@ -23,12 +23,11 @@ export default Vue.extend({
   methods: {
     async loadScores() {
       const scores = await ApiService.getScores();
-      this.scores = scores;
-
-      this.scores
-        .sort((a, b) => b.score - a.score)
-        .forEach((score, index) => {
+      this.scores = scores
+        .sort((a, b) => (a.score > b.score ? -1 : 1))
+        .map((score, index) => {
           score.rank = index + 1;
+          return score;
         });
     },
   },
