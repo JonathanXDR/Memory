77574b7caa5c6177b3c99b932a8aaf58e36a0ed2
Jonathan Russ <jonathan.russ@bluewin.ch>
2022-09-17 20:18:35 +0200

    Update CardTile component

--- src/components/CardTile/CardTile.vue
+++ src/components/CardTile/CardTile.vue
@@ -1,5 +1,11 @@
 <template>
-  <div></div>
+  <div class="tile" :class="{ flipped: card.flipped, found: card.found }">
+    <div
+      class="back-face"
+      :style="`background-image: url('${card.url}')`"
+    ></div>
+    <div class="front-face" @click="$emit('clickCallback')"></div>
+  </div>
 </template>
 
 <script lang="ts" src="./CardTile.ts"></script>
