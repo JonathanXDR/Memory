886d5befabba28a5634e5876ba6a711d2e5e0fca
Jonathan Russ <jonathan.russ@bluewin.ch>
2022-10-04 11:55:40 +0200

    prevent cheating

--- src/components/CardItem/CardItem.vue
+++ src/components/CardItem/CardItem.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="card-item" :class="{ flipped: card.flipped, found: card.found }">
-    <div class="back" :style="`background-image: url('${card.url}')`"></div>
+    <div class="back" :style="`background-image: url('${card.flipped ? card.url : ''}')`"></div>
     <div class="front" @click="$emit('clickCallback')"></div>
   </div>
 </template>
