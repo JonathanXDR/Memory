8cbcb7f162df6dd02333dbbf32b666e14bcb5bf0
Jonathan Russ <jonathan.russ@bluewin.ch>
2022-11-08 19:55:01 +0100

    Update CardItem component

--- src/components/CardItem/CardItem.vue
+++ src/components/CardItem/CardItem.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="card-item" :class="{ flipped: card.flipped, found: card.found }">
-    <div class="back" :style="`background-image: url('${card.flipped ? card.url : ''}')`"></div>
+    <div
+      class="back"
+      :style="`background-image: url('${card.flipped | card.found ? card.url : ''}')`"
+    ></div>
     <div class="front" @click="$emit('clickCallback')"></div>
   </div>
 </template>
