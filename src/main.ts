87ff7acf8203074bafb9e103ca4e9fca6d5e938e
Jonathan Russ <jonathan.russ@bluewin.ch>
2024-04-25 00:48:02 +0200

    Update boilerplate files

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,11 @@
-import { createApp } from "vue";
-import { createPinia } from "pinia";
+import './assets/main.css'
 
-import App from "./App.vue";
-import router from "./router";
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
 
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+const app = createApp(App)
 
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
-import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
-import { faGithub } from "@fortawesome/free-brands-svg-icons";
-import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+app.use(router)
 
-library.add(faEnvelope, faLinkedin, faGithub, faTwitter);
-
-import "./assets/main.css";
-
-const app = createApp(App);
-
-app.use(createPinia());
-app.use(router);
-app.component("font-awesome-icon", FontAwesomeIcon);
-
-app.mount("#app");
+app.mount('#app')
